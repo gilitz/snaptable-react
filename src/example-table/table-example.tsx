abd736e6e2c74a9dabb96519f747141600cfc289
Gilush <gil.itzhaky@gmail.com>
2024-08-04 04:45:00 +0300

    updated package name

--- src/example-table/table-example.tsx
+++ src/example-table/table-example.tsx
@@ -3,7 +3,7 @@ import { Table } from "./table";
 // import { Table as SmartTable } from '../table/table';
 
 export const TableExample = (() => {
-	const dataTable = useDataTable({ key: 'gil-table', columns: tableColumns, saveLayoutView: true })
+	const dataTable = useDataTable({ key: 'gilitz-table', columns: tableColumns, saveLayoutView: true })
 	return (
 		<Table dataTable={dataTable} data={mockData} />
 	)
