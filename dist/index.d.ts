e504b877446d0e0a434c952784dca36e29f8f135
Gilush <gil.itzhaky@gmail.com>
2024-08-05 02:31:17 +0300

    cleanup

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,5 +3,5 @@ import SnapTableType from "../src/types/table-type";
 
 declare module 'snaptable-react' {
 	export const SnapTable: SnapTableType; 
-	export const useDataTable: DataTableType; // Replace `any` with actual type if available
+	export const useDataTable: DataTableType;
   }
