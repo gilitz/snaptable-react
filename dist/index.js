e9aefef861bd57a3a3f23868daf8fd6559521cdb
Gilush <gil.itzhaky@gmail.com>
2024-08-04 23:26:54 +0300

    Restored dist index and version update

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,3 @@
-export  { default as SnapTable } from './../src/table/table';
+export { default as SnapTable } from './../src/table/table';
 export { default as useDataTable } from './../src/hooks/use-data-table';
 export { default as SnapTableType } from './../src/types/table-type';
